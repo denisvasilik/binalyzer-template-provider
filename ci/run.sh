e6a084b753c409a330a739f376fe8a697ad634fe
Denis Vasilik <contact@denisvasilik.com>
2020-07-10 18:44:36 +0200

    Use v0.0.8

--- ci/run.sh
+++ ci/run.sh
@@ -8,8 +8,8 @@ then
     sed -i 's/__tag__ = ""/__tag__ = "'${TRAVIS_TAG}'"/g' binalyzer_template_provider/__init__.py
     sed -i 's/__tag__ = ""/__tag__ = "'${TRAVIS_TAG}'"/g' setup.py
 else
-    sed -i 's/__tag__ = ""/__tag__ = "'0.0.7a${TRAVIS_BUILD_NUMBER}'"/g' binalyzer_template_provider/__init__.py
-    sed -i 's/__tag__ = ""/__tag__ = "'0.0.7a${TRAVIS_BUILD_NUMBER}'"/g' setup.py
+    sed -i 's/__tag__ = ""/__tag__ = "'0.0.8a${TRAVIS_BUILD_NUMBER}'"/g' binalyzer_template_provider/__init__.py
+    sed -i 's/__tag__ = ""/__tag__ = "'0.0.8a${TRAVIS_BUILD_NUMBER}'"/g' setup.py
 fi
 
 if [ ! -z "$TRAVIS_BUILD_NUMBER" ]
