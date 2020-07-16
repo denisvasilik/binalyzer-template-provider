c73acafd7cfffe5b59bac8a2d65603329d6832de
Denis Vasilik <contact@denisvasilik.com>
2020-07-16 22:38:39 +0200

    Add unit tests

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,89 +1,152 @@
 import unittest
 import antlr4
 
-from binalyzer_core import Template
+from binalyzer_core import (
+    Template,
+    RelativeOffsetValueProperty,
+    AutoSizeValueProperty,
+    ValueProperty,
+    BackedBindingContext,
+)
 from binalyzer_template_provider import XMLTemplateParser, XMLTemplateFileParser
 from binalyzer_template_provider.xml import create_input_stream
 
 
-class TemplateElementTestCase(unittest.TestCase):
-    def test_without_attributes(self):
-        template = XMLTemplateParser(
-            """
-            <template>
-            </template>
+def test_default_template_mapping():
+    template = XMLTemplateParser(
         """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
+        <template>
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.name is None
+    assert template.children == ()
+    assert template.parent is None
+    assert template.absolute_address == 0
+    assert template.offset == 0
+    assert isinstance(template.offset_property, RelativeOffsetValueProperty)
+    assert template.size == 0
+    assert isinstance(template.size_property, AutoSizeValueProperty)
+    assert template.padding_before == 0
+    assert isinstance(template.padding_before_property, ValueProperty)
+    assert template.padding_after == 0
+    assert isinstance(template.padding_after_property, ValueProperty)
+    assert template.boundary == 0
+    assert isinstance(template.boundary_property, ValueProperty)
+    assert template.value == bytes()
+    assert isinstance(template.binding_context, BackedBindingContext)
 
-    def test_id_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template name="test-template0">
-            </template>
-        """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
-        self.assertEqual(template.name, "test-template0")
 
-    def test_offset_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template offset="0x100">
-            </template>
+def test_name_attribute():
+    template = XMLTemplateParser(
         """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
-        self.assertEqual(template.offset, 0x100)
+        <template name="test-template0">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.name == "test-template0"
 
-    def test_addressing_mode_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template addressing-mode="absolute">
-            </template>
-        """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
 
-    def test_size_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template size="512">
-            </template>
+def test_offset_attribute():
+    template = XMLTemplateParser(
         """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
-        self.assertEqual(template.size, 512)
+        <template offset="0x100">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.offset == 0x100
+    assert isinstance(template.offset_property, RelativeOffsetValueProperty)
 
-    def test_boundary_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template boundary="0x800">
-            </template>
-        """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
-        self.assertEqual(template.boundary, 0x800)
 
-    def test_padding_before_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template padding-before="0x100">
-            </template>
+def test_addressing_mode_absolute_without_offset_attribute():
+    template = XMLTemplateParser(
         """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
-        self.assertEqual(template.padding_before, 0x100)
+        <template addressing-mode="absolute">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert isinstance(template.offset_property, RelativeOffsetValueProperty)
 
-    def test_padding_after_attribute(self):
-        template = XMLTemplateParser(
-            """
-            <template padding-after="0x400">
-            </template>
+
+def test_addressing_mode_absolute_with_offset_attribute():
+    template = XMLTemplateParser(
         """
-        ).parse()
-        self.assertTrue(isinstance(template, Template))
-        self.assertEqual(template.padding_after, 0x400)
+        <template offset="0x1" addressing-mode="absolute">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert isinstance(template.offset_property, ValueProperty)
+
+
+def test_addressing_mode_relative_without_offset_attribute():
+    template = XMLTemplateParser(
+        """
+        <template addressing-mode="relative">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert isinstance(template.offset_property, RelativeOffsetValueProperty)
+
+
+def test_addressing_mode_relative_with_offset_attribute():
+    template = XMLTemplateParser(
+        """
+        <template offset="0x1" addressing-mode="relative">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert isinstance(template.offset_property, RelativeOffsetValueProperty)
+
+
+def test_size_attribute():
+    template = XMLTemplateParser(
+        """
+        <template size="512">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.size == 512
+
+
+def test_boundary_attribute():
+    template = XMLTemplateParser(
+        """
+        <template boundary="0x800">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.boundary == 0x800
+
+
+def test_padding_before_attribute():
+    template = XMLTemplateParser(
+        """
+        <template padding-before="0x100">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.padding_before == 0x100
+
+
+def test_padding_after_attribute():
+    template = XMLTemplateParser(
+        """
+        <template padding-after="0x400">
+        </template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert template.padding_after == 0x400
 
 
 class FieldElementTestCase(unittest.TestCase):
