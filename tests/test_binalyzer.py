d56060f531396c21b771164e598b9c025fcc09e8
Denis Vasilik <contact@denisvasilik.com>
2020-07-18 10:10:07 +0200

    Add unit tests

--- tests/test_binalyzer.py
+++ tests/test_binalyzer.py
@@ -1,13 +1,13 @@
 """
-    test_binalyzer.py
-    ~~~~~~~~~~~~~~~~~
+    test_binalyzer
+    ~~~~~~~~~~~~~~
 
     This module implements higher level tests for scenarios such as data
     dissection, analysis and generation.
 """
 import io
 import os
-import unittest
+import pytest
 
 
 from anytree import find_by_attr
@@ -33,13 +33,19 @@ def assertStreamEqual(first, second):
             assert first_byte == second_byte
 
 
-def test_data_generation():
+@pytest.fixture
+def binalyzer():
     binalyzer = Binalyzer()
-    binalyzer.template = XMLTemplateParser(
+    XMLTemplateProviderExtension(binalyzer)
+    return binalyzer
+
+
+def test_data_generation(binalyzer):
+    binalyzer.xml.from_str(
         """
-        <template name="template0">
-            <layout name="layout0">
-                <area name="area0">
+        <template>
+            <layout name="layout">
+                <area name="area">
                     <field name="field0" size="32"></field>
                     <field name="field1" size="32"></field>
                     <field name="field2" size="32"></field>
@@ -48,22 +54,48 @@ def test_data_generation():
             </layout>
         </template>
     """
-    ).parse()
-    area = binalyzer.template.layout0.area0
-    area.children[0].value = bytes([0] * 32)
-    area.children[1].value = bytes([1] * 32)
-    area.children[2].value = bytes([2] * 32)
-    area.children[3].value = bytes([3] * 32)
+    )
+    binalyzer.template.layout.area.field0.value = bytes([0] * 32)
+    binalyzer.template.layout.area.field1.value = bytes([1] * 32)
+    binalyzer.template.layout.area.field2.value = bytes([2] * 32)
+    binalyzer.template.layout.area.field3.value = bytes([3] * 32)
 
     assert binalyzer.template.size == 128
     assertStreamEqual(binalyzer.data, TEST_DATA_STREAM_128)
 
 
-def test_data_analysis():
-    cwd_path = os.path.dirname(os.path.abspath(__file__))
+def test_analysis_of_static_template(binalyzer):
+    header_size = 0x100
+    data_size = 0x10
+    footer_size = 0x100
 
-    binalyzer = Binalyzer()
-    XMLTemplateProviderExtension(binalyzer)
+    binalyzer.xml.from_str(
+        f"""
+        <template>
+            <section name="header" size="{header_size}"></section>
+            <section name="data" size="{data_size}"></section>
+            <section name="footer" size="{footer_size}"></section>
+        </template>
+    """
+    )
+
+    binalyzer.data = io.BytesIO(
+        bytes([0x03] * header_size)
+        + bytes([0x05] * data_size)
+        + bytes([0x03] * footer_size)
+    )
+
+    assert binalyzer.template.size == (header_size + data_size + footer_size)
+    assert binalyzer.template.header.size == header_size
+    assert binalyzer.template.header.value == bytes([0x03] * header_size)
+    assert binalyzer.template.data.size == data_size
+    assert binalyzer.template.data.value == bytes([0x05] * data_size)
+    assert binalyzer.template.footer.size == footer_size
+    assert binalyzer.template.footer.value == bytes([0x03] * footer_size)
+
+
+def test_analysis_of_dynamic_template(binalyzer):
+    cwd_path = os.path.dirname(os.path.abspath(__file__))
     binalyzer.xml.from_file(
         os.path.join(cwd_path, "resources/wasm_module.xml"),
         os.path.join(cwd_path, "resources/wasm_module.wasm"),
@@ -74,3 +106,19 @@ def test_data_analysis():
     assert binalyzer.template.magic.value == bytes([0x00, 0x61, 0x73, 0x6D])
     assert binalyzer.template.version.value == bytes([0x01, 0x00, 0x00, 0x00])
     assert instructions.value == bytes([0x01, 0x0B])
+
+
+def test_data_modification(binalyzer):
+    cwd_path = os.path.dirname(os.path.abspath(__file__))
+    binalyzer.xml.from_file(
+        os.path.join(cwd_path, "resources/wasm_module.xml"),
+        os.path.join(cwd_path, "resources/wasm_module.wasm"),
+    )
+
+    instructions = binalyzer.template.code_section.code.function.instructions
+
+    instructions.value = bytes([0x0B, 0x00])
+
+    assert binalyzer.template.magic.value == bytes([0x00, 0x61, 0x73, 0x6D])
+    assert binalyzer.template.version.value == bytes([0x01, 0x00, 0x00, 0x00])
+    assert instructions.value == bytes([0x0B, 0x00])
--- tests/test_boundary.py
+++ tests/test_boundary.py
@@ -2,7 +2,15 @@
     test_boundary
     ~~~~~~~~~~~~~
 
-    This module implements tests related to a template's boundary.
+    This module implements tests related to a template's boundary. A boundary
+    attribute influences the following attributes:
+
+    * Offset of current template
+    * Absolute address of current template
+    * Size of current template
+    * Size of parent template
+
+    NOTE: The current template is the template where the boundary is declared.
 """
 import io
 
@@ -13,69 +21,113 @@ from binalyzer_template_provider import XMLTemplateParser
 def test_boundary_attribute():
     template = XMLTemplateParser(
         """
-        <template boundary="0x800">
-        </template>
+        <template boundary="0x800"></template>
     """
     ).parse()
     assert isinstance(template, Template)
-    assert template.boundary == 0x800
     assert isinstance(template.boundary_property, ValueProperty)
+    assert template.offset == 0
+    assert template.boundary == 0x800
 
 
-def test_boundary_and_matching_parent_offset():
+def test_boundary_attribute_nested():
     template = XMLTemplateParser(
         """
-        <template name="template0">
-            <layout name="layout0" offset="0x200">
-                <area name="area0" boundary="0x200">
+        <template boundary="0x200">
+            <template boundary="0x100"></template>
+        </template>
+    """
+    ).parse()
+    assert template.absolute_address == 0
+    assert template.offset == 0
+    assert template.size == 0x200
+    assert template.children[0].absolute_address == 0
+    assert template.children[0].offset == 0
+    assert template.children[0].size == 0x100
+
+
+def test_boundary_attribute_nested_with_inner_element_having_greater_boundary():
+    template = XMLTemplateParser(
+        """
+        <template boundary="0x200">
+            <template boundary="0x500"></template>
+        </template>
+    """
+    ).parse()
+    assert template.absolute_address == 0
+    assert template.offset == 0
+    assert template.size == 0x600
+    assert template.children[0].absolute_address == 0
+    assert template.children[0].offset == 0
+    assert template.children[0].size == 0x500
+
+
+def test_boundary_attribute_with_offset():
+    template = XMLTemplateParser(
+        """
+        <template offset="0x20" boundary="0x100"></template>
+    """
+    ).parse()
+    assert isinstance(template, Template)
+    assert isinstance(template.boundary_property, ValueProperty)
+    assert template.offset == 32
+    assert template.size == 256
+
+
+def test_boundary_attribute_with_parent_offset_on_boundary():
+    template = XMLTemplateParser(
+        """
+        <template>
+            <layout name="layout" offset="0x200">
+                <area name="area" boundary="0x200"></area>
+            </layout>
+        </template>
+    """
+    ).parse()
+    assert template.layout.area.absolute_address == 0x200
+    assert template.layout.offset == 0x200
+    assert template.layout.area.offset == 0x0
+    assert template.layout.area.boundary == 0x200
+    assert template.layout.area.size == 0x200
+
+
+def test_boundary_attribute_with_parent_offset_besides_boundary():
+    template = XMLTemplateParser(
+        """
+        <template>
+            <layout name="layout" offset="0x300">
+                <area name="area" boundary="0x200">
                 </area>
             </layout>
         </template>"""
     ).parse()
-    assert template.layout0.offset == 0x200
-    assert template.layout0.area0.offset == 0x0
-    assert template.layout0.area0.absolute_address == 0x200
-    assert template.layout0.area0.boundary == 0x200
-    assert template.layout0.area0.size == 0x200
+    assert template.layout.offset == 0x300
+    assert template.layout.area.offset == 0x100
+    assert template.layout.area.absolute_address == 0x400
+    assert template.layout.area.boundary == 0x200
+    assert template.layout.area.size == 0x200
 
 
-def test_boundary_and_not_matching_parent_offset():
+def test_boundary_attribute_with_parent_offset_besides_boundary_and_nested():
     template = XMLTemplateParser(
         """
-        <template name="template0">
-            <layout name="layout0" offset="0x300">
-                <area name="area0" boundary="0x200">
+        <template>
+            <layout name="layout" offset="0x300">
+                <area name="area" boundary="0x200">
+                    <field name="field" boundary="0x100"></field>
                 </area>
             </layout>
-        </template>"""
+        </template>
+    """
     ).parse()
-    assert template.layout0.offset == 0x300
-    assert template.layout0.area0.offset == 0x100
-    assert template.layout0.area0.absolute_address == 0x400
-    assert template.layout0.area0.boundary == 0x200
-    assert template.layout0.area0.size == 0x200
+    assert template.layout.area.absolute_address == 0x400
+    assert template.layout.area.offset == 0x100
+    assert template.layout.area.size == 0x200
+    assert template.layout.area.field.size == 0x100
+    assert template.layout.area.field.offset == 0x0
 
 
-def test_nested_boundary():
-    template = XMLTemplateParser(
-        """
-        <template name="template0">
-            <layout name="layout0" offset="0x300">
-                <area name="area0" boundary="0x200">
-                    <field name="field0" boundary="0x100">
-                    </field>
-                </area>
-            </layout>
-        </template>"""
-    ).parse()
-    assert template.layout0.area0.offset == 0x100
-    assert template.layout0.area0.absolute_address == 0x400
-    assert template.layout0.area0.size == 0x200
-    assert template.layout0.area0.field0.size == 0x100
-    assert template.layout0.area0.field0.offset == 0x0
-
-
-def test_root_template_no_offset_and_boundary():
+def test_boundary_and_sizing_stretch():
     template = XMLTemplateParser(
         """
         <template boundary="0x100">
@@ -96,7 +148,7 @@ def test_root_template_no_offset_and_boundary():
     assert binalyzer.template.payload.value == bytes([0x01] * 252)
 
 
-def test_root_template_offset_and_boundary():
+def test_boundary_and_sizing_stretch_with_offset():
     template = XMLTemplateParser(
         """
         <template offset="0x20" boundary="0x100">
