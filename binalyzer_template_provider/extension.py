7ef9fde2daa876cbabd0deee7388738cbbc2ff41
Denis Vasilik <contact@denisvasilik.com>
2021-03-11 21:01:01 +0100

    from_str returns binalyzer object

--- binalyzer_template_provider/extension.py
+++ binalyzer_template_provider/extension.py
@@ -39,8 +39,10 @@ class XMLTemplateProviderExtension(BinalyzerExtension):
         return self.from_str(template_response.text, data_response.content)
 
     def from_str(self, text: str, data: Optional[bytes] = None):
+        """Reads an XML string and creates a template object model.
+        """
         template = XMLTemplateParser(text, binalyzer=self.binalyzer).parse()
         if data:
             self.binalyzer.data = io.BytesIO(data)
         self.binalyzer.template = template
-        return self.binalyzer.template
+        return self.binalyzer
